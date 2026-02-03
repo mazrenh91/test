f8fc3e7704ac64f1c4a583af9e528ef097c31c1b
Muhammad Azren Hasnen <muhammadazrenhasnen@Azrens-MacBook-Pro-M4.local>
2026-02-03 12:43:36 +0800

    first commit

---
+++ py/01-variables_data_types.py
@@ -0,0 +1,23 @@
+# name = "Azren"        # String
+# age = 35            # Integer
+# weight = 67.0       # Float
+# is_female = False   # Boolean
+
+# print(name)
+# print(type(weight))
+
+# x = 10
+# y = 3
+
+# print(x + y)       # Addition
+# print(x - y)       # Subtraction
+# print(x * y)       # Multiplication
+# print(x / y)       # Division
+# print(x // y)      # Floor division
+# print(x % y)       # Modulus
+# print(x ** y)       # Exponentiation
+
+# celcius = 25
+# fahrenheit = celcius * 9/5 + 32
+
+# print(fahrenheit)
